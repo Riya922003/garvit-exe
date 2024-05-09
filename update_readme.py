bede2b7a8eb77cf1eb9f4ff61f14856320d35679
Riya Gupta <139098932+Riya922003@users.noreply.github.com>
2024-05-09 23:42:52 +0530

    Create update_readme.py

---
+++ update_readme.py
@@ -0,0 +1,25 @@
+import random
+import re
+
+# List of available themes
+themes = ['default', 'radical', 'merko', 'gruvbox', 'tokyonight', 'onedark', 'cobalt', 'synthwave', 'highcontrast', 'dracula']
+
+# Generate a random theme
+new_theme = random.choice(themes)
+
+# Read the README file
+with open('README.md', 'r') as file:
+    readme_content = file.read()
+
+# Update the GitHub Stats image URLs with the new theme
+readme_content = re.sub(r'(https://github-readme-stats-garvit-exe\.vercel\.app/api\?username=garvit-exe&theme=)[^\s]+', rf'\1{new_theme}', readme_content)
+
+# Update the Top Languages image URLs with the new theme
+readme_content = re.sub(r'(https://github-readme-stats-garvit-exe\.vercel\.app/api/top-langs/\?username=garvit-exe&theme=)[^\s]+', rf'\1{new_theme}', readme_content)
+
+# Update the GitHub Streak image URLs with the new theme
+readme_content = re.sub(r'(https://github-readme-streak-stats\.herokuapp\.com\?user=garvit-exe&theme=)[^\s]+', rf'\1{new_theme}', readme_content)
+
+# Write the updated README content back to the file
+with open('README.md', 'w') as file:
+    file.write(readme_content)
