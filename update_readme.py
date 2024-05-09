b1cfafef6476912b956c687632281f5f1c2ff5cb
Riya Gupta <139098932+Riya922003@users.noreply.github.com>
2024-05-10 01:37:19 +0530

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -1,25 +1,14 @@
 import random
 import re
 
-# List of available themes
-themes = ['react']
+# Define the old and new image URLs
+old_image_url = 'https://github-readme-streak-stats.herokuapp.com?user=garvit-exe&theme=transparent&date_format=j%20M%5B%20Y%5D&hide_border=true'
+new_image_url = 'https://github-readme-streak-stats.herokuapp.com?user=garvit-exe&theme=react&date_format=j%20M%5B%20Y%5D&hide_border=true'
 
-# Generate a random theme
-new_theme = random.choice(themes)
-
-# Read the README file
-with open('README.md', 'r') as file:
-    readme_content = file.read()
-
-# Update the GitHub Stats image URLs with the new theme
-readme_content = re.sub(r'(https://github-readme-stats-garvit-exe\.vercel\.app/api\?username=garvit-exe&theme=)[^\s]+', rf'\1{new_theme}', readme_content)
-
-# Update the Top Languages image URLs with the new theme
-readme_content = re.sub(r'(https://github-readme-stats-garvit-exe\.vercel\.app/api/top-langs/\?username=garvit-exe&theme=)[^\s]+', rf'\1{new_theme}', readme_content)
-
-# Update the GitHub Streak image URLs with the new theme
-readme_content = re.sub(r'(https://github-readme-streak-stats\.herokuapp\.com\?user=garvit-exe&theme=)[^\s]+', rf'\1{new_theme}', readme_content)
+# Replace the old image URL with the new one
+readme_content = readme_content.replace(old_image_url, new_image_url)
 
 # Write the updated README content back to the file
 with open('README.md', 'w') as file:
     file.write(readme_content)
+
